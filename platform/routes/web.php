e5b226c624b74300491a682b42d773035a43ac4f
Gerson Vinicius <117097430+gersonvsigna@users.noreply.github.com>
2023-03-27 04:03:17 -0300

    feat: rotas de alunos

--- platform/routes/web.php
+++ platform/routes/web.php
@@ -20,3 +20,13 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['prefix' => 'admin', 'middleware' => ['auth']], function () {
+    Route::get('alunos', 'App\Http\Controllers\AlunoController@index')->name('alunos.index');
+    Route::get('alunos/create', 'App\Http\Controllers\AlunoController@create')->name('alunos.create');
+    Route::post('alunos', 'App\Http\Controllers\AlunoController@store')->name('alunos.store');
+    Route::get('alunos/{id}', 'App\Http\Controllers\AlunoController@show')->name('alunos.show');
+    Route::get('alunos/{id}/edit', 'App\Http\Controllers\AlunoController@edit')->name('alunos.edit');
+    Route::put('alunos/{id}', 'App\Http\Controllers\AlunoController@update')->name('alunos.update');
+    Route::delete('alunos/{id}', 'App\Http\Controllers\AlunoController@destroy')->name('alunos.destroy');
+});
