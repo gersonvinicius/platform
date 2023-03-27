27151e54ffe52f45f4b370b16c0a173fc9631a04
Gerson Vinicius <117097430+gersonvsigna@users.noreply.github.com>
2023-03-27 03:53:07 -0300

    feat: criada a model de alunos

---
+++ platform/app/Models/Aluno.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
+class Aluno extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['nome', 'email', 'data_nascimento'];
+    protected $dates = ['deleted_at'];
+
+    public function cursos()
+    {
+        return $this->belongsToMany(Curso::class);
+    }
+}
